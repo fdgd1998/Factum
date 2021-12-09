f33690b5ee90d245c172e4c32e2a1d741788f032
Francisco Gálvez <afgalvezdiaz@gmail.com>
2021-12-10 00:48:40 +0100

    Fixed code

---
+++ scripts/factura/datos_empresa.php
@@ -0,0 +1,9 @@
+<?php
+    $nombreEmpresa = "Nombre de la empresa";
+    $nifEmpresa = "00000000X";
+    $direccionEmpresa = "Calle Falsa, 123. 00000 Ciudad";
+    $telefonoEmpresa = "+34 600 00 00 00";
+    $emailEmpresa = "contacto@misitio.es";
+    $webEmpresa = "misitio.es";
+    $ibanEmpresa = "ES00 0000 0000 0000 0000";
+?>
