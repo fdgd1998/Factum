165e4246b83f6199a1ec7ae1a374233eb7464ca4
Francisco Gálvez <afgalvezdiaz@gmail.com>
2021-12-10 00:32:06 +0100

    Delete datos_empresa.php

--- scripts/factura/datos_empresa.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $nombreEmpresa = "Nombre  empresa";
-    $nifEmpresa = "00000000X";
-    $direccionEmpresa = "Calle Falsa, 123. 0000 Ciudad";
-    $telefonoEmpresa = "+34 600 00 00 00";
-    $emailEmpresa = "test@example.com";
-    $webEmpresa = "mysite.com";
-?>
