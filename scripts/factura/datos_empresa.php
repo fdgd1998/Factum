b0ade88fe09b8e4c1fe88e61bb7be0fa751fc404
Francisco Gálvez <afgalvezdiaz@gmail.com>
2021-12-10 00:30:54 +0100

    Added datos_empresa to .gitignore

--- scripts/factura/datos_empresa.php
+++ scripts/factura/datos_empresa.php
@@ -1,5 +1,5 @@
 <?php
-    $nombreEmpresa = "Nombre empresa";
+    $nombreEmpresa = "Nombre  empresa";
     $nifEmpresa = "00000000X";
     $direccionEmpresa = "Calle Falsa, 123. 0000 Ciudad";
     $telefonoEmpresa = "+34 600 00 00 00";
