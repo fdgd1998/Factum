9e6b19752b43e4873fa07b1dbfce5a86a8bb605d
Francisco Gálvez <afgalvezdiaz@gmail.com>
2021-12-10 00:33:52 +0100

    Added datos_empresa.php

---
+++ datos_empresa.php
@@ -0,0 +1,8 @@
+<?php
+    $nombreEmpresa = "Nombre empresa";
+    $nifEmpresa = "00000000X";
+    $direccionEmpresa = "Calle False, 123. 00000 Ciudad";
+    $telefonoEmpresa = "+34 600 00 00 00";
+    $emailEmpresa = "test@example.es";
+    $webEmpresa = "mysite.es";
+?>
